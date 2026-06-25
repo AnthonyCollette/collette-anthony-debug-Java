32741ba58bb843c124be2934a87fdae0740772cc
Anthony Collette <100pour100inde@gmail.com>
2026-06-25 13:47:27 +0200

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,16 +3,53 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
-	
-	public static void main(String args[]) throws Exception {
+    private ISymptomReader reader;
+    private ISymptomWriter writer;
+    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    };
+    public List<String> getSymptoms() {
+        return reader.GetSymptoms();
+    };
+    public Map<String, Integer> countSymptoms(List<String> symptoms) {
+        Map<String, Integer> results = new HashMap<>();
+
+        for (String symptom : symptoms) {
+            if (results.containsKey(symptom)) {
+                results.put(symptom, results.get(symptom) + 1);
+            } else {
+                results.put(symptom, 1);
+            }
+        };
+
+        return results;
+    };
+    public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
+        Map<String, Integer> sortedResults = new TreeMap<String, Integer>(symptoms);
+        return sortedResults;
+    };
+    public void writeSymptoms(Map<String, Integer> symptoms) {
+        writer.writeSymptoms(symptoms);
+    };
+
+    public static void main(String args[]) throws Exception {
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
+
+        ReadSymptomDataFromFile symptomsReader = new ReadSymptomDataFromFile("symptoms.txt");
+
+        System.out.println(symptomsReader.GetSymptoms());
 
 		int i = 0;
 		while (line != null) {
@@ -33,6 +70,8 @@ public class AnalyticsCounter {
 
 			line = reader.readLine();	// get another symptom
 		}
+
+
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
